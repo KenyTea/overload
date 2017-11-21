34ef67b121f89cb3df148c10fdc49770146bc37c
Васильева <@223_16>
2017-11-21 19:38:09 +0600

    Фибаначи 2

--- ConsoleApplication23/21,11,17.cpp
+++ ConsoleApplication23/21,11,17.cpp
@@ -11,17 +11,42 @@ int fib(int n) {
 	return fib(n - 1) + fib(n - 2);
 }
 
+int fib2(const int n) {
+	int *a = new int[n + 1];
+	a[0] = 1;
+	a[1] = 1;
+
+	for (int i = 2; i < n; i++) {
+		a[i] = a[i - 1] + a[i - 2];
+	}
+	int res = a[n - 1];
+	delete a;
+	return res;
+
+}
+
+
+
+/*int fib( int N) //Фибаначи
+{
+	//if (N <= 1)
+		//return N;
+	
+	int prevprev = 0, prev = 1;
+	for (int i = 2; i <= N; i++)
+	{
+		int current = prev + prevprev;
+		prevprev = prev;
+		 prev = current;
+	}
+
+	return prev ;
+	//int Result = fib(N - 1) + fib(N - 2);
+	// return result;*/
 
 int main() {
-	/*int a = 0, b = 1, c;
-	cout << "Enter a & b" << endl;
-	cin >> a >> b;
-	for (int i = 2; i < 20; i++) {
-		c = a + b;
-		a = b;
-		b = c;
-		cout << a << " " << b;
-	}*/
+	
+	/*cout << fib(10) << endl;*/
 
 	for (int i = 0; i < 20; i++)
 		cout << "Fibanachi " << i << "     " << fib(i) << endl;
