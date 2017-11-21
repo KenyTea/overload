46f6b8e0ca91cfa7655ed068deb0132b818efee6
Васильева <@223_16>
2017-11-21 20:09:20 +0600

    конструктор перегрузки

--- ConsoleApplication23/21,11,17.cpp
+++ ConsoleApplication23/21,11,17.cpp
@@ -1,60 +1,60 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
- 
-int fib(int n) {
-	if (n == 0 || n == 1) {
-		return n;
-	}
-	return fib(n - 1) + fib(n - 2);
-}
-
-int fib2(const int n) {
-	int *a = new int[n];
-	a[0] = 1;
-	a[1] = 1;
-
-	for (int i = 2; i < n; i++) {
-		a[i] = a[i - 1] + a[i - 2];
-	}
-	int res = a[n - 1];
-	delete a;
-	return res;
-
-}
-
-
-
-/*int fib( int N) //Фибаначи
-{
-	//if (N <= 1)
-		//return N;
-	
-	int prevprev = 0, prev = 1;
-	for (int i = 2; i <= N; i++)
-	{
-		int current = prev + prevprev;
-		prevprev = prev;
-		 prev = current;
-	}
-
-	return prev ;
-	//int Result = fib(N - 1) + fib(N - 2);
-	// return result;*/
-
-int main() {
-	
-	/*cout << fib(10) << endl;*/
-
-	for (int i = 0; i < 20; i++)
-		cout << "Fibanachi " << i << "     " << fib(i) << endl;
-	cout << endl;
-	cout << endl;
-	
-	cout << "fin = " << fib(19) << endl;
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+// 
+//int fib(int n) {
+//	if (n == 0 || n == 1) {
+//		return n;
+//	}
+//	return fib(n - 1) + fib(n - 2);
+//}
+//
+//int fib2(const int n) {
+//	int *a = new int[n];
+//	a[0] = 1;
+//	a[1] = 1;
+//
+//	for (int i = 2; i < n; i++) {
+//		a[i] = a[i - 1] + a[i - 2];
+//	}
+//	int res = a[n - 1];
+//	delete a;
+//	return res;
+//
+//}
+//
+//
+//
+///*int fib( int N) //Фибаначи
+//{
+//	//if (N <= 1)
+//		//return N;
+//	
+//	int prevprev = 0, prev = 1;
+//	for (int i = 2; i <= N; i++)
+//	{
+//		int current = prev + prevprev;
+//		prevprev = prev;
+//		 prev = current;
+//	}
+//
+//	return prev ;
+//	//int Result = fib(N - 1) + fib(N - 2);
+//	// return result;*/
+//
+//int main() {
+//	
+//	/*cout << fib(10) << endl;*/
+//
+//	for (int i = 0; i < 20; i++)
+//		cout << "Fibanachi " << i << "     " << fib(i) << endl;
+//	cout << endl;
+//	cout << endl;
+//	
+//	cout << "fin = " << fib(19) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ ConsoleApplication23/Source3.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Integer {
+public:
+	int value;
+	explicit Integer(){}
+	explicit Integer(int x) : value (x) {}
+
+	Integer & operator = (const int & x) {
+		value = x;
+		return *this;
+	}
+
+	
+
+	Integer & operator + (Integer & x) {
+
+		Integer y(this->value + x.value);
+		return y;
+	}
+
+	Integer & operator - (Integer & x) {
+
+		Integer y(this->value - x.value);
+		return y;
+	}
+
+	/*void operator ++ () {
+		++this->value;
+	}*/
+
+	  Integer(const Integer && i) { // конструктор переноса konstruktor perenosa
+		value = move(i.value);
+	}
+
+	Integer & operator ++ () {
+		++this->value;      // оператор перегрузки инкримента (префиксный)
+		return *this;
+	}
+
+	Integer & operator ++ (int) {
+		Integer old(value);  // оператор перегрузки инкримента (постфиксный)
+		value++;
+		return old;
+	}
+
+	bool operator == (const Integer & x) { // перегрузка ==
+		return value == x.value;
+    }
+
+	bool operator != (const Integer & x) { // перегрузка ==
+		return value != x.value;
+	}
+
+	bool operator > (const Integer & x) { // перегрузка ==
+		return value > x.value;
+	}
+
+	bool operator < (const Integer & x) { // перегрузка ==
+		return value < x.value;
+	}
+};
+
+
+int main() {
+	Integer a(5);
+	Integer b(6);
+
+	Integer c = a + b;
+	Integer d = a - b;
+	cout << c.value << endl;
+	cout << d.value << endl;
+	cout << endl;
+	cout << ++a.value << endl;
+	cout << endl;
+	cout << a.value++ << endl;
+	cout << a.value << endl;
+
+	Integer A(5), B(5), C(6);
+	cout << endl;
+	cout << (A == B) << endl;
+	cout << (A == C) << endl;
+
+	Integer b = move(a); // конструктор переноса konstruktor perenosa
+
+
+
+	return 0;
+}
